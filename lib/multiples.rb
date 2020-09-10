1d45dfb21ac2096ce9f38ed63f19f61beca2c407
JacksonBey <jacksongrowson@gmail.com>
2020-09-10 09:38:09 -0700

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,31 @@
 # Enter your procedural solution here!
+
+def collect_multiples(limit)
+    nums = []
+    arr =*(1...limit)
+    arr.each do |num|
+        if num % 3 == 0
+            nums << num
+        elsif num % 5 ==0
+            nums << num
+        end
+    end
+    return nums
+end
+
+def sum_multiples(limit)
+    # nums = []
+    # arr =*(1...limit)
+    # arr.each do |num|
+    #     if num % 3 == 0
+    #         nums << num
+    #     elsif num % 5 ==0
+    #         nums << num
+    #     end
+    # end
+    # return nums.sum
+    collect_multiples(limit).sum
+end
+
+# nums.reduce(:+)
+# nums.sum
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,26 @@
 # Enter your object-oriented solution here!
+class Multiples
+    
+    attr_reader :range
+
+    def initialize(range)
+        @arr = (1...range)
+        @nums = []
+    end
+
+    def collect_multiples
+        @arr.each do |num|
+            if num % 3 == 0
+                @nums << num
+            elsif num % 5 ==0
+                @nums << num
+            end
+        end
+        return @nums
+    end
+
+    def sum_multiples
+        collect_multiples.sum
+        #@nums.sum
+    end
+end
